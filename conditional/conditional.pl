93c56e3bc8bd36e528b7ab2f48ade05cbdaba081
Salim Djerbouh <tbitw31@gmail.com>
2017-01-17 23:45:21 +0100

    Moved conditional :octocat:

---
+++ conditional/conditional.pl
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+use diagnostics;
+
+use feature 'say';
+
+use feature "switch";
+
+# False undef 0 0.0 "" "0"
+
+my $age = 80;
+my $is_not_intoxicated = 1;
+my $age_last_exam = 16;
+
+if($age < 16){
+	say "You can't drive";
+} elsif(!$is_not_intoxicated){
+	say "You can't drive";
+} else {
+	say "You can drive";
+}
+
+if (($age >= 1) && ($age < 16)){
+	say "You can't drive";
+} elsif(!$is_not_intoxicated){
+	say "You can't drive";
+} elsif(($age >= 80) && (($age > 100) || (($age - $age_last_exam) > 5))){
+	say "You can't drive";
+} else {
+	say "You can drive";
+}
