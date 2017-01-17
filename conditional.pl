a342a2f63d7b0c807d70b8ef0408545ffe96a96a
Salim Djerbouh <tbitw31@gmail.com>
2017-01-17 22:29:41 +0100

    Indent :+1:

--- conditional.pl
+++ conditional.pl
@@ -13,19 +13,19 @@ my $is_not_intoxicated = 1;
 my $age_last_exam = 16;
 
 if($age < 16){
-    say "You can't drive";
+	say "You can't drive";
 } elsif(!$is_not_intoxicated){
-    say "You can't drive";
+	say "You can't drive";
 } else {
-    say "You can drive";
+	say "You can drive";
 }
 
 if (($age >= 1) && ($age < 16)){
-    say "You can't drive";
+	say "You can't drive";
 } elsif(!$is_not_intoxicated){
-    say "You can't drive";
+	say "You can't drive";
 } elsif(($age >= 80) && (($age > 100) || (($age - $age_last_exam) > 5))){
-    say "You can't drive";
+	say "You can't drive";
 } else {
-    say "You can drive";
+	say "You can drive";
 }
