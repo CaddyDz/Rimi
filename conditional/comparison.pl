4aef5b4ca69f4d3a298f2c29ffa2cc223b7b0f2d
Salim Djerbouh <tbitw31@gmail.com>
2017-01-17 23:43:41 +0100

    Strings comparison :dancer:

---
+++ conditional/comparison.pl
@@ -0,0 +1,28 @@
+use strict;
+use warnings;
+use diagnostics;
+
+use feature 'say';
+
+use feature "switch";
+
+use v5.20;
+
+my $age = 80;
+my $is_not_intoxicated = 1;
+my $age_last_exam = 16;
+
+if('a' eq 'b'){
+	say "a equals b";
+} else {
+	say "a doesn't equal b";
+}
+
+# Opposite of if
+unless(!$is_not_intoxicated){
+	say "Get Sober";
+}
+
+# Ternary operator
+
+say (($age > 18) ? "Can Vote" : "Can't Vote");
