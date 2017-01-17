03237ba7535bef5d9de33a0b015f57f6fde44fc4
Salim Djerbouh <tbitw31@gmail.com>
2017-01-17 23:46:56 +0100

    Guess game :globe_with_meridians:

---
+++ loop/guess.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use diagnostics;
+
+use feature 'say';
+
+use feature "switch";
+
+use v5.20;
+
+my $i = 1;
+
+my $lucky_num = 7;
+
+my $guess;
+
+do{
+	say "Guess a Number Between 1 and 10";
+	$guess = <STDIN>;
+} while($guess != $lucky_num);
+
+say "You guessed 7!";
