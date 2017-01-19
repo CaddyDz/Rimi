9f38a6cf8d3c2335266c845426894c2a74623d30
Salim Djerbouh <tbitw31@gmail.com>
2017-01-19 18:28:58 +0100

    Object Oriented

---
+++ oop.pl
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+use diagnostics;
+
+use feature 'say';
+
+use feature "switch";
+
+use v5.20;
+
+use lib 'lib';
+
+use Animal::Cat;
+
+my $whiskers = new Animal::Cat("whiskers", "Salim");
+
+say $whiskers->getName();
+
+$whiskers->setName("Whiskers");
+
+say $whiskers->getName();
+
+say $whiskers->getSound();
