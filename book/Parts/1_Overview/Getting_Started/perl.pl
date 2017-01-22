d2ece92432e7ad7d181a5df758cd2c83457f5a0b
Salim Djerbouh <tbitw31@gmail.com>
2017-01-22 22:44:06 +0100

    test

---
+++ book/Parts/1_Overview/Getting_Started/perl.pl
@@ -0,0 +1,3 @@
+# No declarations needs for simple perl programs unlike other languages like C#
+print "Howdy, world!\n";
+# The above example prints "Howdy, world!" with a new line after that
---
+++ book/Parts/1_Overview/Getting_Started/variables.pl
@@ -0,0 +1,14 @@
+# We call a singular variable a `scalar` and a plural variable an `array`, and since a string can be stored in a scalar variable, we might write a slightly longer (and commented) version of our first example like this:
+my $phrase = "Howdy, world!\n"; # Create a variable.
+print $phrase; # Print new variable.
+# The `my` keyword tells Perl that the variable proceeding it is a brand new one so it doesn't have to go anf look for one with that name
+# Data types doesn't have to be defined explicitly but Perl is not "Dynamically typed language" but uses the sigil "$" in the case of scalars to define the data type for the value
+
+
+
+# Some variable types and their uses
+$cents # Scalar (an individual value be it a number or a string)
+@large # Array (A list of values, keyed by number)
+%interest # Hash (A group of values, keyed by string)
+&how # Subroutine (A callable chunk of Perl code)
+*struck #
