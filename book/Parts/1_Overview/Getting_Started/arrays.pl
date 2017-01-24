160d4c042bf5d61d14f40da25794f65e862b5a73
Salim Djerbouh <tbitw31@gmail.com>
2017-01-24 23:29:02 +0100

    Delete emacs generated files

--- book/Parts/1_Overview/Getting_Started/arrays.pl
+++ book/Parts/1_Overview/Getting_Started/arrays.pl
@@ -1,2 +1,8 @@
 # Arrays in Perl are ordered lists of scalars that are indexed and accessed by number.
 my @home = ("couch", "chair", "table", "stove");
+# As in C, arrays are zero-based
+my @home;
+$home[0] = "couch";
+$home[1] = "chair";
+$home[2] = "table";
+$home[3] = "stove";
--- book/Parts/1_Overview/Getting_Started/perl.c
+++
@@ -1,4 +0,0 @@
-#include "stdio.h"
-int main(){
-	printf("Howdy, world!\n");
-}
