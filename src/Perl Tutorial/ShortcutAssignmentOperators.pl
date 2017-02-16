82ea11a0f32ea62540d87c7af92e2abc620e9fd4
Salim Djerbouh <tbitw31@gmail.com>
2017-02-16 19:20:54 +0100

    Short hand notation Incrementing values with short hand notation Decrementing values with the short hand notation

---
+++ src/Perl Tutorial/ShortcutAssignmentOperators.pl
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+use diagnostics;
+
+use feature 'say';
+
+use feature "switch";
+
+use v5.20;
+
+my $rand_num = 5;
+$rand_num += 1;
+
+say "Number Incremented ", $rand_num;
+
+say "6++ = ", $rand_num++;
+say "++6 = ", ++$rand_num;
+say "6-- = ", $rand_num--;
+say "--6 = ", --6;
