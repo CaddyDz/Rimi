8223b55431f304efd0c7b6f3642ab85b6dab7901
Salim Djerbouh <tbitw31@gmail.com>
2017-02-20 20:59:06 +0100

    lcfirst function

--- src/Perl Tutorial/CaseConversion.pl
+++ src/Perl Tutorial/CaseConversion.pl
@@ -8,8 +8,8 @@ use feature "switch";
 
 use v5.20;
 
-
 my $long_string = "Random long string that isn't that long";
 printf("Uppercase : %s \n", uc $long_string);
 printf("Lowercase : %s \n", lc $long_string);
 printf("1st Uppercase : %s \n", ucfirst $long_string);
+printf("1st Lowercase : %s \n", lcfirst $long_string);
