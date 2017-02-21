a057527609b71283d198ebcbb0fc7b7b71705b4f
Salim Djerbouh <tbitw31@gmail.com>
2017-02-22 00:03:06 +0100

    Numeric literals

---
+++ src/book/Parts/2- The Gory Details/Chapter 2 Bits and Pieces/Numeric Literals.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+use diagnostics;
+use utf8;
+
+use feature 'say';
+
+my $x = 12345						# integer
+my $x = 12345.67;				# floating point
+my $x = 6.02e23;				# scientific notation
+my $x = 4_294_967_296;	# underline for legibility
+my $x = 0377;						# octal
+my $x = 0xffff;					# hexadecimal
+my $x = 0b1100_0000;		# binary
+
+
