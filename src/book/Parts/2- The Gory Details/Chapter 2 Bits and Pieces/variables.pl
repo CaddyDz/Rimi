0cb2c45700e87961ae84a205a07804c2d6447b3d
Salim Djerbouh <tbitw31@gmail.com>
2017-02-21 18:19:16 +0100

    Confusion

--- src/book/Parts/2- The Gory Details/Chapter 2 Bits and Pieces/variables.pl
+++ src/book/Parts/2- The Gory Details/Chapter 2 Bits and Pieces/variables.pl
@@ -1,10 +1,10 @@
 # Access scalar values
 $days = "Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday";
 print $days, "\n";
-@days_array = (1,2);
-print $days_array[1], "\n";
-%days_hash = {
+@days = (1,"stuff");
+print $days[1], "\n";
+%days = {
 	"Jan"=>"January",
 	"Feb"=>"February"
 };
-print %days_hash["Feb"];
+print %days["Feb"];
